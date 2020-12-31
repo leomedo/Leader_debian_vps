26b6b4a1cd6379659ae3a3e5d027c7c35ecee104
Me-D00 <58310128+Me-D00@users.noreply.github.com>
2020-12-31 17:28:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Leader_debian_vps
+* Script Installasi SSH Debian 9 64bit
+--------
+```
+wget https://raw.githubusercontent.com/Me-D00/sulaimanssh/master/debian9/openssh.sh && chmod +x openssh.sh && bash openssh.sh
+```
+
+* Script Installasi OpenVPN Debian 9 64bit
+--------
+```
+wget https://raw.githubusercontent.com/Me-D00/sulaimanssh/master/debian9/openvpn.sh && chmod +x openvpn.sh && bash openvpn.sh
+```
+
+
+* Script Installasi Wiregaud kombine SSH
+--------
+```
+wget https://raw.githubusercontent.com/Me-D00/sulaimanssh/master/debian9/deb9-wg.sh && chmod +x deb9-wg.sh && bash deb9-wg.sh
+
+```
+* Script Installasi Wiregaud kombine SSH
+--------
+```
+wget https://raw.githubusercontent.com/Me-D00/sulaimanssh/master/debian9/wg.sh && chmod +x wg.sh && bash wg.sh
+```
+and Manejemne 
+--------
+```
+wget https://raw.githubusercontent.com/Me-D00/sulaimanssh/master/debian9/manwg.sh && chmod +x manwg.sh && bash manwg.sh
+
+```
---
+++ usernew.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+# Script auto create user SSH
+
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " masaaktif
+
+IP=`curl icanhazip.com`
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "====SSH Account===="
+echo -e "====  Premium Leader SSH   ===="
+echo -e "Host              : $IP" 
+echo -e "Port OpenSSH      : 22,143"
+echo -e "Dropbear          : 44,77"
+echo -e "SSL/TLS           : 443"
+echo -e "SSL/TLS OpenSSH   : 43"
+echo -e "SSL/TLS Dropbear  : 222,777,444"
+echo -e "SSL/TLS SSR       : 69"
+echo -e "Speed Server      : 2 Gbps"
+echo -e "Transfer          : 2 TB"
+echo -e "Username          : $Login "
+echo -e "Password          : $Pass"
+echo -e "-----------------------------"
+echo -e "Aktif Sampai      : $exp"
+echo -e "============================="
+
+echo -e "Mod by LeoMedo"
+echo -e ""
